87a6a774262f99379ab49cc84a6d889f1cc58834
whatisaProCoder <104326867+whatisaProCoder@users.noreply.github.com>
2022-05-02 02:16:30 +0530

    almost finish

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
 export default function App() {
- 
+  const [decimal, setDecimal] = React.useState(0);
+
+  function handleChange(e) {
+    setDecimal(parseInt(e.target.value, 10));
+  }
+
+  function handleIncrement() {
+    setDecimal((prev) => prev + 1);
+  }
+
+  function handleDecrement() {
+    setDecimal((prev) => prev - 1);
+  }
+
+  const binary = Number(decimal).toString(2);
+  const octal = Number(decimal).toString(8);
+  const hexadecimal = Number(decimal).toString(16).toUpperCase();
 
   return (
     <div className="App">
@@ -15,20 +31,26 @@ export default function App() {
         label="Enter Decimal"
         variant="outlined"
         margin="normal"
-        color="success"
+        value={decimal === 0 ? "" : decimal}
+        onChange={handleChange}
       />
       <div className="button-group">
-        <Button variant="contained">
+        <Button variant="contained" onClick={handleIncrement}>
           Increment
         </Button>
-        <Button variant="contained">
+        <Button variant="contained" onClick={handleDecrement}>
           Decrement
         </Button>
-        <Button variant="outlined" color="error">
+        <Button variant="outlined" color="error" onClick={() => setDecimal(0)}>
           Reset
         </Button>
       </div>
-      <Typography variant="h3">1000</Typography>
+      <h4>Binary</h4>
+      <Typography variant="h3">{binary}</Typography>
+      <h4>Octal</h4>
+      <Typography variant="h3">{octal}</Typography>
+      <h4>Hexadecimal</h4>
+      <Typography variant="h3">{hexadecimal}</Typography>
     </div>
   );
 }
